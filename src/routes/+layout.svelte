245ad9996e3fe8313217ec05fbb4d6ea1e591b68
hactuss <157395485+hactuss@users.noreply.github.com>
2025-11-02 14:00:17 +0100

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import favicon from "$lib/assets/doggo.gif";
+    import "../app.css";
+    let { children } = $props();
+</script>
+
+<svelte:head>
+    <link rel="icon" href={favicon} />
+    <title>hactuss linktree</title>
+</svelte:head>
+
+{@render children?.()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+</script>
+
+<main id="main-container">
+    <div>┏============================┓</div>
+    <div>
+        <br />
+        <h1>Hactuss</h1>
+        <br />
+        <img
+            src="https://media1.tenor.com/m/mc3OyxhLazUAAAAC/doggo-doge.gif"
+            alt=""
+            width="150px"
+        />
+        <br />
+        <a href="https://hactuss.vercel.app">Homepage</a>
+        <a href="https://youtube.com/@hactuss">YouTube</a>
+        <a href="https://hactuss.newgrounds.com">Newgrounds</a>
+        <a href="https://steamcommunity.com/profiles/76561199810742102">Steam</a
+        >
+        <a href="https://github.com/hactuss">Github</a>
+        <br />
+        <p>passion projects</p>
+        <a href="https://isitspookymonth.vercel.app">is it spooky month?</a>
+        <a href="https://hactuss.github.io/pages/office/office.html"
+            >worldclock</a
+        >
+    </div>
+    <div>┗============================┛</div>
+</main>
+
+<!-- ▎▒-->
+<style>
+    #main-container {
+        margin-top: 50px;
+        background-color: black;
+    }
+</style>
