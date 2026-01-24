91e004a25ad08c6a95f42c13f99011a8506b7299
hactuss <hactuss@proton.me>
2026-01-24 11:56:47 +0100

    modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/routes/+layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
-	import favicon from '$lib/assets/doggo.gif';
-	import '../app.scss';
-	let { children } = $props();
+    import favicon from "$lib/assets/doggo.gif";
+    import "../app.scss";
+    let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
-	<title>hactuss linktree</title>
+    <link rel="icon" href={favicon} />
+    <link
+        rel="stylesheet"
+        href="https://raw.githubusercontent.com/sindresorhus/modern-normalize/refs/heads/main/modern-normalize.css"
+    />
+    <title>hactuss linktree</title>
 </svelte:head>
 
 {@render children?.()}
